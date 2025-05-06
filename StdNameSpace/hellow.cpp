032204b6e26be45d3dd9fb013931c94dbc054a37
imrangit07 <imranhussain21309@gmail.com>
2025-05-06 14:51:38 +0530

    Add DSA first code

---
+++ StdNameSpace/hellow.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello, Imran";
+    return 0;
+}
+
+
+/*1. PREPROCESSOR DIRECTIVE;
+
+    #- This symbole indicates a preprocessor directive (processed before actual compilation)
+    
+    include- Tells the preprocessor to include the contents of another file
+    
+    <iostream>- The standard C++ header file for input/output operations (class)
+
+*/ 
+
+
+/* 
+
+    namespace- it is a collection of identifier
+
+      using namespace std; brings all names from the standard namespace into the current
+      scope, allowing you to use them without the std:: prefix
+
+      std - std is th estandard namespace
+      :: - Scope resolution operator
+
+*/
+
+/*
+   main
+   -Every C++ program must have exactly one main function
+   -It's where program execution begins
+   -The operationg system calls main when the program starts
+   -The int return type is a convention to communicate success (0) or failure (non-Zero) to 
+     the operation system
+
+   {- Begins the function's body(scope)
+   }- Ends the function's body(scope)
+
+   -Everything between these is executed when the function is called
+*/
+
+/*
+    << - Insertion Operator
+
+         
+
+*/
